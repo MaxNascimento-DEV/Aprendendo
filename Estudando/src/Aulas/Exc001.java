d0dc05b806da9fd2eb417f6ae1cf9e3ed91c1eaf
MaxNascimento-DEV <maxwellcadete9@gmail.com>
2026-03-02 16:43:44 -0300

    Adicionado novo Exc com POO

--- Estudando/src/Aulas/Exc001.java
+++ Estudando/src/Aulas/Exc001.java
@@ -2,10 +2,12 @@ package Aulas;
 
 public class Exc001 {
         public void main(String[] args) {
-            int numberA = 5;
-            int numberB = 4;
-            int soma = numberA + numberB;
 
-            System.out.println(soma);
+            Exc002 pessoa =  new Exc002(21, "Max", 2003);
+
+            System.out.println(pessoa.getNumber());
+            System.out.println(pessoa.getName());
+            System.out.println(pessoa.getYears());
+
         }
 }
--- Estudando/src/Aulas/Exc002.java
+++ Estudando/src/Aulas/Exc002.java
@@ -1,4 +1,40 @@
 package Aulas;
 
 public class Exc002 {
+
+      private int number;
+      private String name;
+      private int years;
+
+      public Exc002(int number, String name, int years) {
+          this.number = number;
+          this.name = name;
+          this.years = years;
+      }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getName(){
+          return name;
+    }
+
+    public void setName(String name){
+          this.name = name;
+    }
+
+    public int getYears(){
+          return years;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
 }
+
