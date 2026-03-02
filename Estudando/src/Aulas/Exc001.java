074ede96e7fecd976097fc4df60910209fb0c422
Maxwell Nascimento <maxwellcadete9@gmail.com>
2026-03-02 16:55:51 -0300

    Update Exc001.java
    Criado uma nova pessoa!

--- Estudando/src/Aulas/Exc001.java
+++ Estudando/src/Aulas/Exc001.java
@@ -4,10 +4,15 @@ public class Exc001 {
         public void main(String[] args) {
 
             Exc002 pessoa =  new Exc002(21, "Max", 2003);
+            Exc002 pessoa1 = new Exc003(25, "Sabrina", 2000);     
 
             System.out.println(pessoa.getNumber());
             System.out.println(pessoa.getName());
             System.out.println(pessoa.getYears());
+
+            System.out.println(pessoa1.getNumber());
+            System.out.println(pessoa1.getName());
+            System.out.println(pessoa1.getYears());
 
         }
 }
